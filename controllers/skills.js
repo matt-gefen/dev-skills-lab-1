432ecedc28dacb1afd24691e9d01b64d6bf55507
mattgefen <matt.gefen@gmail.com>
2021-10-26 17:42:58 -0600

    skills controller

---
+++ controllers/skills.js
@@ -0,0 +1,26 @@
+import * as skillsDb from "../data/skills-db.js
+
+function index(req, res) {
+  skillsDb.find({}, function(error, skills) {
+    res.render('skills/index', {
+      skills: skills,
+      error: error,
+      title: "I May Know Some Things!"
+    })
+  })
+}
+
+function show(req, res) {
+  skillsDb.findById(req.params.id, function(error, skill) {
+    res.render('skills/show', {
+      skill: skill,
+      error: error
+    })
+  })
+}
+
+
+export {
+	index,
+  show
+}
